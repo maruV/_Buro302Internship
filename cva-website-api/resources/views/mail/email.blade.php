bb664f27247e6591aae24636a9dfa90ccbd0e4b8
Vishal maru <maruvishal35@gmail.com>
2021-05-21 20:27:23 +0530

    Delete cva-website-api directory

--- cva-website-api/resources/views/mail/email.blade.php
+++
@@ -1,5 +0,0 @@
-<p>
-    Maak het registratie process af door naar de volgende link to navigeren:
-    <br>
-    <a href="http://www.cvaketenzorg.nl/login/{{$token}}">http://www.cvaketenzorg.nl/login/{{$token}}</a>
-</p>
